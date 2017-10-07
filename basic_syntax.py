08d1cf4f6d76c8949f7cf6e613e1e65cf45acbe4
jakobpederson <jakob.pederson+github@gmail.com>
2017-10-07 10:54:53 -0500

    second basic syntax problem

--- basic_syntax.py
+++ basic_syntax.py
@@ -2,11 +2,27 @@
 # Basic Syntax
 # Write a program that asks the user for a number (integer only)
 # and prints the sum of its digits
+from collections import Counter
 
 def sum_digits(integer_only):
     string = str(integer_only)
     numbers = [int(x) for x in string]
     return sum(numbers)
+
+
+"""
+Write program that takes a file name as command line argument,
+count how many times each word appears in the file and prints
+the word that appears the most (and its relevant count)
+"""
+def most_common_word(file_path):
+    with open(file_path, 'r') as f:
+        data = f.readlines()
+    result = data[0].split()
+    c = Counter(result)
+    if c.most_common(1):
+        return c.most_common(1)[0]
+    return "No words"
 
 if __name__ == "__main__":
     from argparse import ArgumentParser
---
+++ test_basic_syntax.py
@@ -0,0 +1,44 @@
+import os
+import unittest
+
+from basic_syntax import sum_digits, most_common_word
+
+
+class BasicSyntaxOneTest(unittest.TestCase):
+
+    def test_sum_digits_returns_correct_answer(self):
+        self.assertEqual(0, sum_digits(0))
+        self.assertEqual(1, sum_digits(1))
+        self.assertEqual(1, sum_digits(10))
+        self.assertEqual(1, sum_digits(100))
+        self.assertEqual(2, sum_digits(20))
+        self.assertEqual(3, sum_digits(12))
+        self.assertEqual(17, sum_digits(98))
+        self.assertEqual(17, sum_digits(89))
+
+
+class BasicSyntaxTwoTest(unittest.TestCase):
+
+
+    def setUp(self):
+        self.FILES = {
+        "test_1.txt": "duck duck duck goose",
+        "test_2.txt": "cat cat",
+        "test_3.txt": " "
+        }
+        self.delete_if_exists()
+        for path, data in self.FILES.items():
+            with open(path, 'w') as f:
+                f.write(data)
+
+    def delete_if_exists(self):
+        for path in self.FILES.keys():
+            try:
+                os.remove(path)
+            except OSError:
+                pass
+
+    def test_gets_most_common_word_and_count(self):
+        self.assertEqual(('duck', 3), most_common_word("test_1.txt"))
+        self.assertEqual(('cat', 2), most_common_word("test_2.txt"))
+        self.assertEqual(("No words"), most_common_word("test_3.txt"))
