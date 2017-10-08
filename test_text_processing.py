454d2b2ce9c2b20c60ac6f4c22edc9d670bb9228
jakobpederson <jakob.pederson+github@gmail.com>
2017-10-08 09:51:00 -0500

    initial text processing

---
+++ test_text_processing.py
@@ -0,0 +1,46 @@
+import os
+import unittest
+
+import text_processing
+
+
+class TextProcessingTest(unittest.TestCase):
+
+    def setUp(self):
+        self.FILES = {
+        "test_1.txt": "lo0: flags=9<up>mtu\noptions=12\ninet 1234\nstatus:active",
+        "test_2.txt": "ex0: flags=9<up>mtu\noptions=12\ninet 5678\n",
+        "test_3.txt": "zo0o: flags=9<up>mtu\noptions=12\ninet 9101\nstatus:active",
+        "test_4.txt": "lo0: flags=9<up>mtu\noptions=12\ninet 1234\nstatus:active\nzo0o: flags=9<up>mtu\noptions=12\ninet 9101\nstatus:active",
+        }
+        self.delete_if_exists()
+        for path, data in self.FILES.items():
+            with open(path, 'w') as f:
+                f.write(data)
+
+    def tearDown(self):
+        self.delete_if_exists()
+
+    def delete_if_exists(self):
+        for path in self.FILES.keys():
+            self.delete_or_pass(path)
+
+    def delete_or_pass(self, path):
+        try:
+            os.remove(path)
+        except OSError:
+            pass
+
+    def test_extracts_network_stats(self):
+        result_1 = text_processing.get_network_stats("test_1.txt")
+        result_2 = text_processing.get_network_stats("test_2.txt")
+        result_3 = text_processing.get_network_stats("test_3.txt")
+        # result_4 = text_processing.get_network_stats("test_4.txt")
+        self.assertCountEqual(["lo0", "1234", "active"], result_1)
+        self.assertCountEqual(["ex0", "5678", ""], result_2)
+        self.assertCountEqual(["zo0o", "9101", "active"], result_3)
+
+        # self.assertCountEqual([
+        #     ["lo0", "1234", "active"],
+        #     ["zo0o", "9101", "active"]
+        # ], result_4)
---
+++ text_processing.py
@@ -0,0 +1,66 @@
+'''
+Given the following input:
+lo0: flags=8049<UP,LOOPBACK,RUNNING,MULTICAST> mtu 16384
+	options=1203<RXCSUM,TXCSUM,TXSTATUS,SW_TIMESTAMP>
+	inet 127.0.0.1 netmask 0xff000000
+	inet6 ::1 prefixlen 128
+	inet6 fe80::1%lo0 prefixlen 64 scopeid 0x1
+	nd6 options=201<PERFORMNUD,DAD>
+gif0: flags=8010<POINTOPOINT,MULTICAST> mtu 1280
+en0: flags=8863<UP,BROADCAST,SMART,RUNNING,SIMPLEX,MULTICAST> mtu 1500
+	ether f4:0f:24:29:df:4d
+	inet6 fe80::1cb5:1689:1826:cc7b%en0 prefixlen 64 secured scopeid 0x4
+	inet 10.176.85.19 netmask 0xffffff00 broadcast 10.176.85.255
+	nd6 options=201<PERFORMNUD,DAD>
+	media: autoselect
+	status: active
+en1: flags=963<UP,BROADCAST,SMART,RUNNING,PROMISC,SIMPLEX> mtu 1500
+	options=60<TSO4,TSO6>
+	ether 06:00:58:62:a3:00
+	media: autoselect <full-duplex>
+	status: inactive
+p2p0: flags=8843<UP,BROADCAST,RUNNING,SIMPLEX,MULTICAST> mtu 2304
+	ether 06:0f:24:29:df:4d
+	media: autoselect
+	status: inactive
+Create a CSV file from the data as follows:
+
+interface,inet,status
+lo0,127.0.0.1,
+gif0,,
+en0,10.176.85.19,active
+en1,,inactive
+p2p0,,inactive
+'''
+from collections import namedtuple
+
+
+Stats = namedtuple("Stats", ["interface", "inet", "status"])
+
+def get_network_stats(file_name):
+    with open(file_name, "r") as f:
+        lines = f.readlines()
+        interface = get_interface(lines)
+        inet = get_inet(lines)
+        status = get_status(lines)
+    return [interface, inet, status]
+
+def get_interface(lines):
+    lst = lines
+    result = []
+    for value in lst:
+        var = value.split(':')
+        if var[0] not in ("status", "media"):
+            return var[0]
+
+def get_inet(lines):
+    for line in lines:
+        if "inet " in line:
+            return line[5:].replace('\n', '')
+
+def get_status(lines):
+    for line in lines:
+        if line.startswith('status:'):
+            return line[7:].replace('\n', '')
+    return ""
+
